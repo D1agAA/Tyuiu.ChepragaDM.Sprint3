95bd940dcd27e17398f60b60ea0a0c9dda136ac2
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-10-26 21:44:26 +0500

    Task1

---
+++ Tyuiu.ChepragaDM.Sprint3.Task1.V4.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ChepragaDM.Sprint3.Task1.V4.Lib
+{
+    public class DataService : ISprint3Task1V4
+    {
+        public double GetSumSeries(int startValue, int stopValue)
+        {
+            double i = startValue ;
+            double res = 0;
+            while (i <= stopValue)
+            {
+                
+                res += Math.Pow(1/i, 2);
+                i++;
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.ChepragaDM.Sprint3.Task1.V4/Program.cs
@@ -0,0 +1,53 @@
+﻿using Tyuiu.ChepragaDM.Sprint3.Task1.V4.Lib;
+
+
+namespace Tyuiu.ChepragaDM.Sprint3.Task1.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Чепрага Д. М. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Чепрага Дмитрий Максимович | ИБКСб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл While, которая вычисляет сумму ряда   *");
+            Console.WriteLine("* по формуле                                                              *");
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+
+            int x, y;
+            x = 1;
+            Console.WriteLine("Начало шага: " + x);
+
+            y = 10;
+            Console.WriteLine("Конец шага: " + y);
+
+           
+
+
+
+
+            double res = ds.GetSumSeries( x, y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
