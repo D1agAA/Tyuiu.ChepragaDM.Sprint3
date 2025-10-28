4049b939992f94d6672ed7ef6b5512b7c2548a65
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-10-28 18:53:55 +0500

    Task4

---
+++ Tyuiu.ChepragaDM.Sprint3.Task4.V10/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.ChepragaDM.Sprint3.Task4.V10.Lib;
+
+
+namespace Tyuiu.ChepragaDM.Sprint3.Task4.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Чепрага Д. М. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Чепрага Дмитрий Максимович | ИБКСб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+            Console.WriteLine("* функции y=(sin(x)-x)/x. При х = 0 пропустить значение. Полученные       *");
+            
+            Console.WriteLine("* значения перемножать.                                                   *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+
+
+            int x, y;
+            x = -5;
+            Console.WriteLine("Начало:  " + x);
+            y = 5;
+            Console.WriteLine("Конец: " + y);
+
+
+
+
+
+
+            double res = ds.Calculate(x, y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
